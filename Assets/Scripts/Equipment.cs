78b45b2365bd97d56da2267530677d49b50248d6
NickPolchies <polchiesnicholas@gmail.com>
2020-06-20 13:48:53 -0300

    Added purchase menu items/consolidated equipment scritps and finished equipment prefabs

--- Assets/Scripts/Equipment.cs
+++ Assets/Scripts/Equipment.cs
@@ -2,9 +2,11 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public abstract class Equipment : MonoBehaviour
+public class Equipment : MonoBehaviour
 {
     public float ThermalRating;
     public int price;
     public int upkeep;
+    public float watts;
+    public float slots;
 }
--- Assets/Scripts/GameUI.cs
+++ Assets/Scripts/GameUI.cs
@@ -1,16 +1,30 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameUI : MonoBehaviour
 {
+    public Equipment[] equipmentList;
+    public Button[] equipmentButtons;
+
     void Start()
     {
-        
+        for(int i = 1; i < equipmentButtons.Length; i++)
+        {
+            TextMeshProUGUI buttonText = equipmentButtons[i].GetComponent<TextMeshProUGUI>();
+            buttonText.text = "COST\n" + equipmentList[i].price;
+        }
     }
 
     void Update()
     {
         
+    }
+
+    void buyItem()
+    {
+
     }
 }

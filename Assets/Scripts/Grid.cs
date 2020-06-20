f2c724c5ac1a0ca15ce405957aef120588eaa605
NickPolchies <polchiesnicholas@gmail.com>
2020-06-20 10:59:26 -0300

    Can add new objects to the grid (with test code)

--- Assets/Scripts/Grid.cs
+++ Assets/Scripts/Grid.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
 
 public class Grid : MonoBehaviour
@@ -7,6 +8,7 @@ public class Grid : MonoBehaviour
     public Transform gridAnchor, gridFarCorner;
     public int width, height, cellWidth, cellHeight;
     private GameObject[,] machines;
+    public GameObject TEST, TEST2;
 
     private void Start()
     {
@@ -18,6 +20,12 @@ public class Grid : MonoBehaviour
         if (Input.GetMouseButtonDown(0))
         {
             Debug.Log(ScreenToGridCoords((Vector2)Input.mousePosition));
+            AddEquipment(ScreenToGridCoords((Vector2)Input.mousePosition), TEST);
+        }
+        if (Input.GetMouseButtonDown(1))
+        {
+            Debug.Log(ScreenToGridCoords((Vector2)Input.mousePosition));
+            AddEquipment(ScreenToGridCoords((Vector2)Input.mousePosition), TEST2);
         }
     }
 
@@ -36,11 +44,25 @@ public class Grid : MonoBehaviour
             return cellHeight * height;
         }
     }
-
-    public void InsertIntoSquare(int col, int row, GameObject g)
+    public void AddEquipment(Vector2Int point, GameObject equipmentPrefab)
     {
-        machines[col,row] = g;
-        g.transform.position = gridAnchor.TransformPoint(new Vector3(col * gridPixelWidth / width, row * gridPixelHeight / height));
+        AddEquipment(point.x, point.y, equipmentPrefab);
+    }
+
+    public void AddEquipment(int col, int row, GameObject equipmentPrefab)
+    {
+        if (col < 0)
+        {
+            return;
+        }
+
+        Destroy(machines[col, row]);
+
+        GameObject equipmentInstance = Instantiate(equipmentPrefab, new Vector3(0, 0, 0), Quaternion.identity);
+        machines[col,row] = equipmentInstance;
+
+        equipmentInstance.transform.parent = gridAnchor;
+        equipmentInstance.transform.position = gridAnchor.TransformPoint(new Vector3(col * gridPixelWidth / width, row * gridPixelHeight / height));
     }
 
     public GameObject GetSquare(int col, int row)
@@ -48,12 +70,12 @@ public class Grid : MonoBehaviour
         return machines[col,row];
     }
 
-    public Vector2 ScreenToGridCoords(Vector2 point)
+    public Vector2Int ScreenToGridCoords(Vector2 point)
     {
         return ScreenToGridCoords(point.x, point.y);
     }
 
-    public Vector2 ScreenToGridCoords(float x, float y)
+    public Vector2Int ScreenToGridCoords(float x, float y)
     {
         Vector3 point = new Vector3(x, y);
         point = Camera.main.ScreenToWorldPoint(point);
@@ -62,13 +84,13 @@ public class Grid : MonoBehaviour
         //Remove gaps between grid squares
         if (FilterX(point.x) || FilterY(point.y))
         {
-            return new Vector2(-1, -1);
+            return new Vector2Int(-1, -1);
         }
 
         int col = (int)(point.x / gridPixelWidth * width);
         int row = (int)(point.y / gridPixelHeight * height);
 
-        return new Vector2(col, row);
+        return new Vector2Int(col, row);
     }
 
     public GameObject GetObjectAtGridCoords(int col, int row)
--- Assets/Scripts/TruckController.cs
+++ Assets/Scripts/TruckController.cs
@@ -6,9 +6,25 @@ public class TruckController : MonoBehaviour
 {
     public int cash;
     public float watts, temperature;
-    // Start is called before the first frame update
+    private Grid grid;
+    public GameObject[] startingEquipment = new GameObject[5];
+
+    private void OnValidate()
+    {
+        if(startingEquipment.Length > 5)
+        {
+            Debug.LogError("ERR: Setting starting equipment to more than 5 will cause runtime errors");
+        }
+    }
+
     void Start()
     {
+        grid = GetComponent<Grid>();
+        for(int i = 0; i < startingEquipment.Length; i++)
+        {
+            grid.AddEquipment(i, 0, startingEquipment[i]);
+        }
+
         
     }
 

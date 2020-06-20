c440ead5c3d13c42a1b9e22ce07cc69606c49815
NickPolchies <polchiesnicholas@gmail.com>
2020-06-20 15:35:10 -0300

    Attempting to get shop working

---
+++ Assets/Scripts/DragButton.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.EventSystems;
+
+public class DragButton : MonoBehaviour, IPointerDownHandler, IPointerUpHandler
+{
+    public Equipment equipment;
+
+    public UnityEvent onDragClick;
+
+    private bool pointerDown;
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        pointerDown = true;
+        Debug.Log("HoldingDown");
+    }
+
+    public void OnPointerUp(PointerEventData eventData)
+    {
+        Debug.Log("Released");
+    }
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        if (pointerDown)
+        {
+
+        }
+    }
+}
--- Assets/Scripts/GameUI.cs
+++ Assets/Scripts/GameUI.cs
@@ -2,30 +2,42 @@
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEngine.EventSystems;
 using UnityEngine.UI;
 
-public class GameUI : MonoBehaviour
+public class GameUI : MonoBehaviour, IPointerDownHandler, IPointerUpHandler
 {
     public Equipment[] equipmentList;
     public Button[] equipmentButtons;
-    public Equipment buying;
     public TruckController truck;
     public EnvironmentInfo environmentInfo;
+    
+    [Header("Menu Text")]
     public TextMeshProUGUI dayText;
+
+    private Equipment buying;
+    private bool mouseDown;
 
     void Start()
     {
         buying = null;
-        for(int i = 1; i < equipmentButtons.Length; i++)
+        for (int i = 1; i < equipmentButtons.Length; i++)
         {
             TextMeshProUGUI buttonText = equipmentButtons[i].GetComponentInChildren<TextMeshProUGUI>();
             buttonText.text = "COST\n" + equipmentList[i].upkeepCost;
-            equipmentButtons[i].onClick.AddListener(() => BuyItem(equipmentList[i]));
+
+            Equipment e = equipmentList[i];
+
+            equipmentButtons[i].onClick.AddListener(() => { Debug.Log("FFFFF"); BuyItem(e); });
+//            equipmentButtons[i]
         }
     }
 
     void Update()
     {
+        if (Input.GetMouseButtonDown(0))
+        {
+        }
         if (Input.GetMouseButtonUp(0) && buying != null)
         {
             Debug.Log("TEST");
@@ -36,7 +48,17 @@ public class GameUI : MonoBehaviour
 
     public void BuyItem(Equipment e)
     {
-        Debug.Log("TEAGAER");
         buying = e;
+        truck.buyEquipment(truck.getMouseGridPosition(), buying);
+    }
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        mouseDown = true;
+    }
+
+    public void OnPointerUp(PointerEventData eventData)
+    {
+
     }
 }

78a5e8e511b931e336fb9f2a01bbb2f91e15766b
NickPolchies <polchiesnicholas@gmail.com>
2020-06-20 00:18:34 -0300

    Working start menu

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class MainMenu : MonoBehaviour
+{
+    public Button startGame;
+    public Button quitGame;
+
+    void Start()
+    {
+        startGame.onClick.AddListener(StartGame);
+        quitGame.onClick.AddListener(QuitGame);
+    }
+
+    void StartGame()
+    {
+        SceneManager.LoadScene("MainGame");
+    }
+
+    void QuitGame()
+    {
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#elif UNITY_WEBPLAYER
+        Application.OpenURL(webplayerQuitURL);
+#else
+        Application.Quit();
+#endif
+    }
+}

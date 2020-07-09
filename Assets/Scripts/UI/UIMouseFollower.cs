509c6d1a711e9417a5e1d86e7924ae01e2162db7
NickPolchies <polchiesnicholas@gmail.com>
2020-07-09 13:05:05 -0300

    Added mouse tracking UI script
    Fixed broken heat particles on deepfrier

---
+++ Assets/Scripts/UI/UIMouseFollower.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIMouseFollower : MonoBehaviour
+{
+    public RectTransform rectTransform;
+
+    void Update()
+    {
+        float width = rectTransform.rect.width;
+        float height = rectTransform.rect.height;
+
+        rectTransform.anchoredPosition = Input.mousePosition;
+    }
+}

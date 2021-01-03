336af84ee0ce4c80d67af0834cb8f65ee4288c39
NickPolchies <polchiesnicholas@gmail.com>
2021-01-03 18:51:54 -0400

    Credits scene now looks like main menu but with credits text
    Credits text is placeholder for now

--- Assets/Scripts/UI/CreditsController.cs
+++ Assets/Scripts/UI/CreditsController.cs
@@ -3,11 +3,12 @@ using UnityEngine.SceneManagement;
 
 public class CreditsController : MonoBehaviour
 {
+    public string mainMenuSceneName;
     void Update()
     {
         if (Input.anyKeyDown)
         {
-            SceneManager.LoadScene("MainMenu");
+            SceneManager.LoadScene(mainMenuSceneName);
         }
     }
 }
